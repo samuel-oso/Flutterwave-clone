77677486ddfafc806a2c08e79b2335658413e9f7
samuel-oso <ososamuel035@gmail.com>
2022-11-14 11:41:50 +0100

    Mobile dropdown and hero section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 $(".menu").click(function () {
   $(this).toggleClass("open");
 });
+
+function buttonClicked() {
+  var x = document.getElementById("nav__dropdown");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
