5ed5ddfd9add84efe0dc4fab1f3d3c3b51358edb
samuel-oso <ososamuel035@gmail.com>
2022-11-15 07:02:43 +0100

    Fixed  mobile nav dropdown height

--- js/script.js
+++ js/script.js
@@ -4,9 +4,13 @@ $(".menu").click(function () {
 
 function buttonClicked() {
   var x = document.getElementById("nav__dropdown");
+  var main = document.getElementById("main");
+
   if (x.style.display === "block") {
     x.style.display = "none";
+    main.style.display = "block";
   } else {
     x.style.display = "block";
+    main.style.display = "none";
   }
 }
