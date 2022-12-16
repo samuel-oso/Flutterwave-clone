2e25b92a5eca40eb706aec14619c8aa87ec8118b
samuel-oso <ososamuel035@gmail.com>
2022-12-16 14:42:35 +0100

    fix: refactored some designs

--- js/script.js
+++ js/script.js
@@ -25,3 +25,45 @@ for (i = 0; i < accordion.length; i++) {
     this.classList.toggle("active");
   });
 }
+
+var $card = $(".card");
+var lastCard = $(".stacked-cards .card").length - 1;
+
+$(".next").click(function () {
+  var prependList = function () {
+    if ($(".card").hasClass("activeNow")) {
+      var $slicedCard = $(".card")
+        .slice(lastCard)
+        .removeClass("transformThis activeNow");
+      $("ul").prepend($slicedCard);
+    }
+  };
+  $("li")
+    .last()
+    .removeClass("transformPrev")
+    .addClass("transformThis")
+    .prev()
+    .addClass("activeNow");
+  setTimeout(function () {
+    prependList();
+  }, 150);
+});
+
+$(".prev").click(function () {
+  var appendToList = function () {
+    if ($(".card").hasClass("activeNow")) {
+      var $slicedCard = $(".card").slice(0, 1).addClass("transformPrev");
+      $(".card-list").append($slicedCard);
+    }
+  };
+
+  $("li")
+    .removeClass("transformPrev")
+    .last()
+    .addClass("activeNow")
+    .prevAll()
+    .removeClass("activeNow");
+  setTimeout(function () {
+    appendToList();
+  }, 150);
+});
